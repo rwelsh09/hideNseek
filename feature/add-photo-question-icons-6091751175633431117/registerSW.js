89d342f1eeb6fe107ed653873e458970b6034c86
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-24 13:47:25 +0000

    deploy: c9ab7fd7afaa413eb792f9960f3d28e17ea8ea35

---
+++ feature/add-photo-question-icons-6091751175633431117/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/feature/add-photo-question-icons-6091751175633431117/sw.js', { scope: '/HideAndSeek/feature/add-photo-question-icons-6091751175633431117/' })})}
