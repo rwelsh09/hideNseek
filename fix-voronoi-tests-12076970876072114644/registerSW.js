db2d4a6b13a26be6e7ac048320763913e682e087
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 21:44:43 +0000

    deploy: d45f388443da8550f5adfe2766dda2854206dcf8

---
+++ fix-voronoi-tests-12076970876072114644/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-voronoi-tests-12076970876072114644/sw.js', { scope: '/HideAndSeek/fix-voronoi-tests-12076970876072114644/' })})}
