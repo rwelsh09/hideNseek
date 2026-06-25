c5af841a9ce3e2462a85f76b6a27ac8ae0901849
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 04:21:42 +0000

    deploy: 3550e5be66f302c6495383e12677dddd59831369

---
+++ feature/live-update-map-default-16737286310824199031/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/feature/live-update-map-default-16737286310824199031/sw.js', { scope: '/HideAndSeek/feature/live-update-map-default-16737286310824199031/' })})}
