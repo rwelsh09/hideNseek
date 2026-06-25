175d254c394fe536b3c42916df750feb203c404b
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 21:53:25 +0000

    deploy: 3d46e0d63782048eea09153e39a7fd2873418216

---
+++ security/fix-xss-pwa-manifest-10606757448870614216/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/security/fix-xss-pwa-manifest-10606757448870614216/sw.js', { scope: '/HideAndSeek/security/fix-xss-pwa-manifest-10606757448870614216/' })})}
