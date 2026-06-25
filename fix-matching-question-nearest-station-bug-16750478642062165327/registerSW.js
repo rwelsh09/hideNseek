3a2e015cc019bccc695d83851940ea0264cdf33d
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 14:35:49 +0000

    deploy: ee279350847ac3784f0c1780a17967f62828c83d

---
+++ fix-matching-question-nearest-station-bug-16750478642062165327/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-matching-question-nearest-station-bug-16750478642062165327/sw.js', { scope: '/HideAndSeek/fix-matching-question-nearest-station-bug-16750478642062165327/' })})}
