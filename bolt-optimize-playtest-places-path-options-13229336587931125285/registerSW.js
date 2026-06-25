37ea53e704edbd33d473bcbd51255ba569602a03
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 11:08:11 +0000

    deploy: 82463afa01c4d2d8513ecb47b6d7066f1b09acb2

---
+++ bolt-optimize-playtest-places-path-options-13229336587931125285/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/bolt-optimize-playtest-places-path-options-13229336587931125285/sw.js', { scope: '/HideAndSeek/bolt-optimize-playtest-places-path-options-13229336587931125285/' })})}
