6b3154923eb6de12673365d678e987ba20161f97
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-25 14:10:13 +0000

    deploy: 8b1221c8f72e87a26d213e6e262255460831f83b

---
+++ feature/paste-question-button-5906811624839833878/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/feature/paste-question-button-5906811624839833878/sw.js', { scope: '/HideAndSeek/feature/paste-question-button-5906811624839833878/' })})}
