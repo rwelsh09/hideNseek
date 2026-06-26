5d9d76fcf5b79e20e0f392d506e689b9a6135714
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 01:27:02 +0000

    deploy: e5eda6844434969ec9691a7a12940b8140af41a7

---
+++ remove-custom-locations-1386172159979962411/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/remove-custom-locations-1386172159979962411/sw.js', { scope: '/HideAndSeek/remove-custom-locations-1386172159979962411/' })})}
