7d64ba8236d693812acf0f05f6c7c8059f5e6998
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 04:56:08 +0000

    deploy: f32c766bdb1623b097e61900e955861ffa3ccc2f

---
+++ fix-rules-copy-11303535996853482242/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-rules-copy-11303535996853482242/sw.js', { scope: '/HideAndSeek/fix-rules-copy-11303535996853482242/' })})}
