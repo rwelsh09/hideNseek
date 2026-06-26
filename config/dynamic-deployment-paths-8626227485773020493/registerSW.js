5e4c1e6bc812fdb5106b8acfb3ddff44d48e06e5
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 03:50:30 +0000

    deploy: 3ff96dc31b68d034cf2b43b23b2979dcfa9c0824

---
+++ config/dynamic-deployment-paths-8626227485773020493/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/config/dynamic-deployment-paths-8626227485773020493/sw.js', { scope: '/HideAndSeek/config/dynamic-deployment-paths-8626227485773020493/' })})}
