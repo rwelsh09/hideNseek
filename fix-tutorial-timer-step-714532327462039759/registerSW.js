9efa67596a8eff313f90fa62b9a49836af7d1794
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 14:37:30 +0000

    deploy: 049bd7b07afdc8206ea3fbaf0f194f95e02633a7

---
+++ fix-tutorial-timer-step-714532327462039759/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-tutorial-timer-step-714532327462039759/sw.js', { scope: '/HideAndSeek/fix-tutorial-timer-step-714532327462039759/' })})}
