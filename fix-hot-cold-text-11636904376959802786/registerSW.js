e7223c94e4d0f30830f7a006f09ed4ae5b561679
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 22:04:35 +0000

    deploy: 5ddf1c0c12c0b359ee9a736d8144582d43f33d90

---
+++ fix-hot-cold-text-11636904376959802786/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-hot-cold-text-11636904376959802786/sw.js', { scope: '/HideAndSeek/fix-hot-cold-text-11636904376959802786/' })})}
