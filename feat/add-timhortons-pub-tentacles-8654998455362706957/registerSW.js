8f53ab92e25d99e77ed1ffa968febd2d3cb3ef4a
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-26 01:13:12 +0000

    deploy: d295652fb89ba06db1eb579b4d3ebc219ad888cd

---
+++ feat/add-timhortons-pub-tentacles-8654998455362706957/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/feat/add-timhortons-pub-tentacles-8654998455362706957/sw.js', { scope: '/HideAndSeek/feat/add-timhortons-pub-tentacles-8654998455362706957/' })})}
