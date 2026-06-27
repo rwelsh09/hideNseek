af3d86c4d487539b22867c7faba234295ed3e8cc
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-27 00:34:32 +0000

    deploy: 962b7f414714dfc909b90c541e355f0f58161a75

---
+++ fix-question-menu-layout-3510012814711976777/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-question-menu-layout-3510012814711976777/sw.js', { scope: '/HideAndSeek/fix-question-menu-layout-3510012814711976777/' })})}
