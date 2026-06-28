f7a76e151d0ccbd0161c2a13e2f2e3450ce0fab6
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-28 13:38:25 +0000

    deploy: ddca9c9dab312c62d845f7ec0fc754cc8b65b15d

---
+++ bolt/memoize-leaflet-refs-1620811930229844798/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/bolt/memoize-leaflet-refs-1620811930229844798/sw.js', { scope: '/HideAndSeek/bolt/memoize-leaflet-refs-1620811930229844798/' })})}
