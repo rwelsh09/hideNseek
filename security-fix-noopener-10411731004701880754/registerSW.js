64629e6aed91df0e829de87304c198a9022aedd4
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-29 03:36:00 +0000

    deploy: 1472a92bfab297d6694b097d5db3d37836082dfc

---
+++ security-fix-noopener-10411731004701880754/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/security-fix-noopener-10411731004701880754/sw.js', { scope: '/HideAndSeek/security-fix-noopener-10411731004701880754/' })})}
