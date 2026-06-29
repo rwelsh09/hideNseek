8e6c8141b57a15270caedebad1abb780cd494a74
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-29 03:42:15 +0000

    deploy: 841f07cc3ec72ae035a2a8183aaa3f5ab4d4b1f0

---
+++ jules-test-geo-api-4982962335906253870/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/jules-test-geo-api-4982962335906253870/sw.js', { scope: '/HideAndSeek/jules-test-geo-api-4982962335906253870/' })})}
