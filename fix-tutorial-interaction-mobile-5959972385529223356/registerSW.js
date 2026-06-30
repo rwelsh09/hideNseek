128ca23a662be173750375c451fcef26c9982b9c
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-06-30 05:33:50 +0000

    deploy: 3af49ad369b9422f57c422c31e535e20eced351a

---
+++ fix-tutorial-interaction-mobile-5959972385529223356/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/fix-tutorial-interaction-mobile-5959972385529223356/sw.js', { scope: '/HideAndSeek/fix-tutorial-interaction-mobile-5959972385529223356/' })})}
