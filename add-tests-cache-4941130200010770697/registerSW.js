f285736205e880a206e8d6a429f719cfad735ff6
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-07-03 17:40:06 +0000

    deploy: fd1ee15975ed449ecac1608f45484d6cc486f25b

---
+++ add-tests-cache-4941130200010770697/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HideAndSeek/add-tests-cache-4941130200010770697/sw.js', { scope: '/HideAndSeek/add-tests-cache-4941130200010770697/' })})}
