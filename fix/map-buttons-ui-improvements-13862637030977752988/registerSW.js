46491b8c3a0a54b28db95b27ff8281a011f7f399
rwelsh09 <rwelsh09@users.noreply.github.com>
2026-07-05 20:13:50 +0000

    deploy: 7c7aae5d9df53549c263cfe888055d0b8a52ea55

---
+++ fix/map-buttons-ui-improvements-13862637030977752988/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hideNseek/fix/map-buttons-ui-improvements-13862637030977752988/sw.js', { scope: '/hideNseek/fix/map-buttons-ui-improvements-13862637030977752988/' })})}
