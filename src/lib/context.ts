347de1f13cfa1bcd1899b40cfcf4935eb03da873
rwelsh09 <rwelsh09@gmail.com>
2026-07-08 07:48:39 -0600

    Sync with `master`
    Add persistent atom for showHiderTutorial

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -56,6 +56,11 @@ export const hasSeenWelcome = persistentAtom<boolean>("hasSeenWelcome", false, {
     decode: JSON.parse,
 });
 
+export const showHiderTutorial = persistentAtom<boolean>("showHiderTutorial", true, {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+});
+
 export const tutorialDriver = atom<any>(null);
 export const headStartMinutes = persistentAtom<number>("headStartMinutes", 45, {
     encode: JSON.stringify,
