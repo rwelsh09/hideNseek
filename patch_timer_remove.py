a0885433db573bb523aa5140c85918e50e1d29c8
rwelsh09 <rwelsh09@gmail.com>
2026-07-10 22:39:03 -0600

    Delete patch_timer_remove.py

--- patch_timer_remove.py
+++
@@ -1,8 +0,0 @@
-with open('src/components/TimerDrawer.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace('            lockedRecommendedStart.set(null);\n', '')
-content = content.replace('        lockedRecommendedStart.set(null);\n', '')
-
-with open('src/components/TimerDrawer.tsx', 'w') as f:
-    f.write(content)
