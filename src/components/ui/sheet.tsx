4643365e2a6492c5db1510362d03145d0e10ff06
rwelsh09 <rwelsh09@gmail.com>
2026-07-16 10:32:41 -0600

    Remove unused Sheet component exports
    Removed unused exports from sheet.tsx

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -126,13 +126,5 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
     Sheet,
-    SheetClose,
     SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetOverlay,
-    SheetPortal,
-    SheetTitle,
-    SheetTrigger,
 };
