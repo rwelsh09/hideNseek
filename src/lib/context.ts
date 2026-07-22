748b613da206a16ffd64c63d5dc0b94be1a1b551
rwelsh09 <rwelsh09@gmail.com>
2026-07-21 20:20:35 -0600

    Increase minimum loading time from 200ms to 300ms

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -193,7 +193,7 @@ if (typeof window !== "undefined" && navigator.permissions) {
 const _isLoading = atom<boolean>(false);
 let loadingTimeout: ReturnType<typeof setTimeout> | null = null;
 let lastLoadingStartTime = 0;
-const MINIMUM_LOADING_TIME = 200; // ms
+const MINIMUM_LOADING_TIME = 300; // ms
 
 export const isLoading = {
     ..._isLoading,
