b80274ecd5d3337a28c03dac00c6f22cdeab428f
rwelsh09 <rwelsh09@gmail.com>
2026-07-29 09:01:09 -0600

    Simplify and clarify QUESTION_RULES descriptions

--- src/lib/rules.ts
+++ src/lib/rules.ts
@@ -1,12 +1,12 @@
 // Used in the `Rules` page & for `How it works` on the Question card
 export const QUESTION_RULES: Record<string, string> = {
     "hot/cold":
-        "Seekers travel a specific distance (e.g., 1km) providing the Hider with their starting and end point. The Hider must reveal if the Seekers are now Warmer (closer to) or Colder (farther from) the Hider's position.",
-    radar: "Seekers select a specific distance (e.g., 5km) to project a Radar from their current location. The Hider must answer 'Inside' or 'Outside' to whether their location falls within that Radar.",
-    match: "Seekers ask if the Hider's location shares a specific attribute with them (e.g., 'Does your station's name start with the same letter as ours?', 'Is your nearest Library the same as ours?').",
+        "Seekers travel a specific distance providing the Hider with their starting and end point. The Hider must reveal if the Seekers are now Warmer (closer to) or Colder (farther from) the Hider's position.",
+    radar: "Seekers select a specific distance to project a Radar from their current location. The Hider must answer 'Inside' or 'Outside' to whether their location falls within that Radar.",
+    match: "Seekers ask if the Hider's location shares a specific attribute with them.",
     measure:
-        "Seekers ask if the Hider is closer to or farther from a specific type of place (e.g. Hospitals) than they are.",
+        "Seekers ask if the Hider is closer to or farther from a specific type of place than they are.",
     closest:
-        "Seekers select a category. The app mathematically divides the map into distinct regions originating from the 5 closest locations to the Seekers' marker. The Hider must reveal which 'closest' (region) contains their location.",
+        "Seekers select a category. The Hider must reveal which of the 5 places is the closest.",
     photo: "Seekers request a photograph from the Hider's current perspective. The photo must accurately represent their location without being intentionally misleading. The Hider may edit the photo to black out words.",
 };
